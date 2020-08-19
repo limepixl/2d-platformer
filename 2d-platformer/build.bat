001303f763f31a700d02cfa050ccac6e20cf22dc
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2020-08-19 10:52:39 +0200

    Removed Visual Studio project files

--- 2d-platformer/build.bat
+++
@@ -1,1 +0,0 @@
-clang -O2 -Wall -Wextra .\src\main.cpp ..\thirdparty\glad\src\glad.c .\src\AssetManagement\*.cpp .\src\Display\*.cpp .\src\Sprite\*.cpp -I ..\thirdparty\glad\include\ -I ..\thirdparty\GLFW\include\ -I ..\thirdparty\glm\ -I ..\thirdparty\ -D_CRT_SECURE_NO_WARNINGS -L ..\thirdparty\GLFW\lib\ -lglfw3-clang -lgdi32 -luser32 -lshell32 -lmsvcrt
